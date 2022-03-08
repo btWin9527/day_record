a62e8f5478ff1bacefca76c58a6d304239406c02
guojiguang <guojiguang@kaikeba.com>
2022-03-08 14:23:40 +0800

    feat: 添加有序链表

---
+++ leetcode/2022_03/02_合并两个有序链表.ts
@@ -0,0 +1,70 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+//
+//
+// 示例 2：
+//
+//
+//输入：l1 = [], l2 = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+//
+// Related Topics 递归 链表 👍 2193 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+// 定义列表结构
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+/**
+ * @method mergeTwoLists 合并两个升序列表
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ * @description 递归 链表
+ *
+ */
+// function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+//     if(list1 === null || list2 === null) return null
+//     for (let i = 0; i < list2.length; i++) {
+//         list2[i]
+//     }
+// }
+//leetcode submit region end(Prohibit modification and deletion)
