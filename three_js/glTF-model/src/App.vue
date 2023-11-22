35422ec76cbbb43f05b5df8cdd5af0214a7faed8
郭吉光 <1135899486@qq.com>
2023-11-22 18:48:13 +0800

    feat: 添加配置文件

---
+++ three_js/glTF-model/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BirdModel from './components/BirdModel/index.vue'
+</script>
+
+<template>
+	<div>
+		<BirdModel></BirdModel>
+	</div>
+
+</template>
+
+<style scoped>
+
+</style>
